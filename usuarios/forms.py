a27457593566f3c4dea5ee696ee47bcae0ed6e5a
Carlos Cutile <carlos.cutile@outlook.com>
2024-07-05 22:12:42 -0300

    se corrigio registro.html

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -3,11 +3,29 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class NuestroFormularioRegistro(UserCreationForm):
-    email = forms.EmailField
-    password1 = forms.CharField(label='Contrasenia', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Repetir Contrasenia', widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'contactus', 'placeholder': 'Email'}))
+    password1 = forms.CharField(label='Contrasenia', widget=forms.PasswordInput(attrs={'class': 'contactus', 'placeholder': 'Contraseña'}))
+    password2 = forms.CharField(label='Repetir Contrasenia', widget=forms.PasswordInput(attrs={'class': 'contactus', 'placeholder': 'Repetir Contraseña'}))
     
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
         help_texts = {key: '' for key in fields}
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'contactus', 'placeholder': 'Usuario'}),
+        }
+
+
+# from django import forms
+# from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.models import User
+
+# class NuestroFormularioRegistro(UserCreationForm):
+#     email = forms.EmailField
+#     password1 = forms.CharField(label='Contrasenia', widget=forms.PasswordInput)
+#     password2 = forms.CharField(label='Repetir Contrasenia', widget=forms.PasswordInput)
+    
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password1', 'password2']
+#         help_texts = {key: '' for key in fields}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login as django_login
 from usuarios.forms import NuestroFormularioRegistro
 
@@ -23,12 +23,31 @@ def user_login(request):
     return render(request, 'usuarios/login.html', {'formulario': formulario})
             
 def registro(request):
-    
-    formulario = NuestroFormularioRegistro
-    
-    if request.method == "POST":
+    if request.method == 'POST':
         formulario = NuestroFormularioRegistro(request.POST)
         if formulario.is_valid():
             formulario.save()
-            return redirect('login')
-    return render(request, 'usuarios/registro.html', {'formulaio': formulario})
+            username = formulario.cleaned_data.get('username')
+            password = formulario.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                django_login(request, user)
+                return redirect('inicio')
+            else:
+                print(formulario.errors)  # Imprimir los errores del formulario
+    else:
+        formulario = NuestroFormularioRegistro()
+    return render(request, 'usuarios/registro.html', {'formulario': formulario})
+
+
+
+# def registro(request):
+    
+#     formulario = NuestroFormularioRegistro
+    
+#     if request.method == "POST":
+#         formulario = NuestroFormularioRegistro(request.POST)
+#         if formulario.is_valid():
+#             formulario.save()
+#             return redirect('login')
+#     return render(request, 'usuarios/registro.html', {'formulaio': formulario})
