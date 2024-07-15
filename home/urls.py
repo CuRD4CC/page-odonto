03c044cc699ea41dc7b12d27414eb035267bbfa6
Carlos Cutile <carlos.cutile@outlook.com>
2024-07-15 04:17:42 -0300

    se terminos los html de cada tratamiento

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,12 @@ urlpatterns = [
     path('contactanos/', views.contacto, name='contactanos'),
     path('tratamientos/', views.tratamiento, name='tratamientos'),
     path('tratamientos/endodoncia/', views.tratamiento_endodoncia, name='endodoncia'),
+    path('tratamientos/cirugia/', views.tratamiento_cirugia, name='cirugia'),
+    path('tratamientos/estetica/', views.tratamiento_estetica, name='estetica'),
+    path('tratamientos/higiene/', views.tratamiento_higiene, name='higiene'),
+    path('tratamientos/implantologia/', views.tratamiento_implantologia, name='implantologia'),
+    path('tratamientos/odontologia/', views.tratamiento_odontologia, name='odontologia'),
+    path('tratamientos/odontopediatria/', views.tratamiento_odontopediatria, name='odontopediatria'),
+    path('tratamientos/ortodoncia/', views.tratamiento_ortodoncia, name='ortodoncia'),
+    path('tratamientos/periodoncia/', views.tratamiento_periodoncia, name='periodoncia'),
 ]
--- home/views.py
+++ home/views.py
@@ -14,3 +14,27 @@ def tratamiento(request):
 
 def tratamiento_endodoncia(request):
     return render(request, 'home/endodoncia.html')
+
+def tratamiento_cirugia(request):
+    return render(request, 'home/cirugia_oral.html')
+
+def tratamiento_estetica(request):
+    return render(request, 'home/estetica_dental.html')
+
+def tratamiento_higiene(request):
+    return render(request, 'home/higiene_bucodental.html')
+
+def tratamiento_implantologia(request):
+    return render(request, 'home/implantologia.html')
+
+def tratamiento_odontologia(request):
+    return render(request, 'home/odontologia_general.html')
+
+def tratamiento_odontopediatria(request):
+    return render(request, 'home/odontopediatria.html')
+
+def tratamiento_ortodoncia(request):
+    return render(request, 'home/ortodoncia.html')
+
+def tratamiento_periodoncia(request):
+    return render(request, 'home/periodoncia.html')
